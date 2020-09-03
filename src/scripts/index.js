e806169940e1acf672fc96e8b5c2655f46807f25
IlhamShiddiq <shdqillham123@gmail.com>
2020-09-03 19:27:50 +0700

    side menu for mobile device

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,10 +3,12 @@ import 'regenerator-runtime'; /* for async await transpile */
 // JS
 import get from './getData.js';
 import getInfo from './getInfo.js';
+import './action.js';
 
 // CSS
 import '../styles/main.css';
 import '../styles/responsive.css';
+import '../styles/mobile-navigation.css';
 import '../styles/font.css';
 
 get();
